41a91f18d9c92b39d1446b3429fe8841b9c4ab66
Sher Minn Chong <sherminn.chong@gmail.com>
2014-09-12 18:00:15 +0800

    Clean CSS and JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,4 @@ $(document).ready(function(){
     //###################################################### RUN CODE #####################################################
     templateInitializerCallback();
 
-
-    //###################################################### GOOGLE ANALYTICS #####################################################
-    // var _gaq=[['_setAccount','UA-43969265-1'],['_trackPageview']];
-    // (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    // g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    // s.parentNode.insertBefore(g,s)}(document,'script'));
 })
