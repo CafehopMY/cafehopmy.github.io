0b21d40a656bfed51ec41fc2e4b4fe31219315a9
piratefsh <sherminn.chong@gmail.com>
2014-09-28 13:34:56 +0800

    Minor details

--- app/map/controllers/map_controller.js
+++ app/map/controllers/map_controller.js
@@ -1,8 +1,8 @@
 angular.module('cafehopApp.controllers').controller('MapController', ['$scope', '$http', function($scope, $http){
     $scope.map = {
         center: {
-            latitude: 45,
-            longitude: -73
+            latitude: 3.136402,
+            longitude: -101.66394
         },
         zoom: 8
     };
