41e1b1a9c3be68092439cb1be4893a814eee605d
piratefsh <sherminn.chong@gmail.com>
2014-09-28 12:30:03 +0800

    Add faq.json

--- app/faq/controllers/faq_controller.js
+++ app/faq/controllers/faq_controller.js
@@ -1,11 +1,10 @@
 angular.module('cafehopApp.controllers').controller('FaqController', ['$scope', '$http', function($scope, $http){
-    var filename ='app/faq/models/faq.txt';
+    var filename ='app/faq/models/faq.json';
     $scope.faq = "Loading FAQ..."; 
 
     $http.get(filename)
         .success(function(data){
-            $scope.faq = data;
-            console.log(data);
+            $scope.faqs = data.faqs;
         })
         .error(function(){
             console.error(filename + ' not found.')
