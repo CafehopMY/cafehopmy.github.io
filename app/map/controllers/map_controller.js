9a702596afe560191461978c509201073b6f1840
piratefsh <sherminn.chong@gmail.com>
2014-09-28 14:07:12 +0800

    Implement finding curr location

--- app/map/controllers/map_controller.js
+++ app/map/controllers/map_controller.js
@@ -1,10 +1,31 @@
 angular.module('cafehopApp.controllers').controller('MapController', ['$scope', '$http', function($scope, $http){
+    $scope.markers = [];
+
+    $scope.ready = function(map){
+        $scope.instance = map;
+        $scope.$apply(function(){
+            // Get current user location
+            if(navigator.geolocation){
+               navigator.geolocation.getCurrentPosition(function(pos){
+                var latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+                $scope.instance.panTo(latlng);
+                $scope.markers.push({
+                    idKey: $scope.markers.length,
+                    coords: pos.coords
+                });
+               });
+            }
+        });
+    }
+
     $scope.map = {
         center: {
             latitude: 3.136402,
-            longitude: -101.66394
+            longitude: 101.66394
         },
-        zoom: 8
+        zoom: 11,
+        events: {
+            tilesloaded: $scope.ready
+        }
     };
-
 }]);
