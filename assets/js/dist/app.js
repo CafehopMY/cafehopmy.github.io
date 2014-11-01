f70faf0bb2ff9e380604c34969720e68ae0e5bd2
Sher Minn Chong <sherminn.chong@gmail.com>
2014-11-01 15:15:50 +0800

    Add active class to active view

--- assets/js/dist/app.js
+++ assets/js/dist/app.js
@@ -407,11 +407,15 @@ angular.module('cafehopApp.controllers').controller('MapController',
     $scope.activateMapView = function(){
         $('.map-view').removeClass('hide-view');
         $('.list-view').addClass('hide-view');
+        $('.list-view-btn').removeClass('active');
+        $('.map-view-btn').addClass('active');
     }
 
     $scope.activateListView = function(){
         $('.map-view').addClass('hide-view');
         $('.list-view').removeClass('hide-view');
+        $('.list-view-btn').addClass('active');
+        $('.map-view-btn').removeClass('active');
 
     }
 
